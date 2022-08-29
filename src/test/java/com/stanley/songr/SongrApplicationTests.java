0a4175465478f6d1c5a6e7232bc7cdf310208520
Stanley <stanley.wilder.24@gmail.com>
2022-08-29 15:32:28 -0700

    initial commit

---
+++ src/test/java/com/stanley/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanley.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
